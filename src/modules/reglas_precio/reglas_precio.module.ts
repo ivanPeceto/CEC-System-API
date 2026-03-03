e9d24fe5262c91bee592ecd1c54e366a6e09516f
ivan <ivanpeceto20@gmail.com>
2026-03-03 18:45:01 -0300

    chore: Configures module

--- src/modules/reglas_precio/reglas_precio.module.ts
+++ src/modules/reglas_precio/reglas_precio.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReglasPrecioService } from './reglas_precio.service';
 import { ReglasPrecioController } from './reglas_precio.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReglasPrecio } from './entities/reglas_precio.entity';
+import { Producto } from '../productos/entities/producto.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReglasPrecio, Producto])],
   controllers: [ReglasPrecioController],
   providers: [ReglasPrecioService],
+  exports: [ReglasPrecioService],
 })
 export class ReglasPrecioModule {}
