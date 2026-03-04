eb93cff9ca001f415ba77b460c2a1121223fef7d
ivan <ivanpeceto20@gmail.com>
2026-03-04 12:54:11 -0300

    chore: Configs pedidos entity module

--- src/modules/pedidos/pedidos.module.ts
+++ src/modules/pedidos/pedidos.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { PedidosController } from './pedidos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pedido } from './entities/pedido.entity';
+import { PedidoProducto } from '../pedido-producto/entities/pedido-producto.entity';
+import { Cliente } from '../clientes/entities/cliente.entity';
+import { PedidoProductoModule } from '../pedido-producto/pedido-producto.module';
+import { ClientesModule } from '../clientes/clientes.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Pedido, PedidoProducto, Cliente]),
+    PedidoProductoModule,
+    ClientesModule,
+  ],
   controllers: [PedidosController],
   providers: [PedidosService],
 })
