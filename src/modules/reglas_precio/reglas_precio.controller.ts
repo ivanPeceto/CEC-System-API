2e25b903f986eb352e9c16df541c592600708806
ivan <ivanpeceto20@gmail.com>
2026-03-04 11:16:13 -0300

    chore: Implements service methods into endpoints

--- src/modules/reglas_precio/reglas_precio.controller.ts
+++ src/modules/reglas_precio/reglas_precio.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ReglasPrecioService } from './reglas_precio.service';
 import { CreateReglasPrecioDto } from './dto/create-reglas_precio.dto';
 import { UpdateReglasPrecioDto } from './dto/update-reglas_precio.dto';
+import { Role } from 'src/common/decorators/roles/roles.decorator';
+import { Roles } from 'src/types/users.types';
 
 @Controller('reglas-precio')
 export class ReglasPrecioController {
@@ -19,16 +29,36 @@ export class ReglasPrecioController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.reglasPrecioService.findOne(+id);
+    return this.reglasPrecioService.findOne(id);
+  }
+
+  @Get('producto/:id')
+  findOneByProducto(@Param('id') id: string) {
+    return this.reglasPrecioService.findOneByProducto(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateReglasPrecioDto: UpdateReglasPrecioDto) {
-    return this.reglasPrecioService.update(+id, updateReglasPrecioDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateReglasPrecioDto: UpdateReglasPrecioDto,
+  ) {
+    return this.reglasPrecioService.update(id, updateReglasPrecioDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.reglasPrecioService.remove(+id);
+  softDelete(@Param('id') id: string) {
+    return this.reglasPrecioService.delete(id);
+  }
+
+  @Role(Roles.ADMIN)
+  @Delete(':id/hard')
+  delete(@Param('id') id: string) {
+    return this.reglasPrecioService.delete(id);
+  }
+
+  @Role(Roles.ADMIN)
+  @Patch('restore/:id')
+  restore(@Param('id') id: string) {
+    return this.reglasPrecioService.restore(id);
   }
 }
